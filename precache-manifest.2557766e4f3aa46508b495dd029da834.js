3674a8c995133e53305ade139f5238516bbc20b1
Budescu Eftimie <budescueftimie@gmail.com>
2022-07-01 05:10:03 +0200

    Updates

---
+++ precache-manifest.2557766e4f3aa46508b495dd029da834.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "84a952125db838f979f9",
+    "url": "/language-app/static/css/main.1c0725e8.chunk.css"
+  },
+  {
+    "revision": "84a952125db838f979f9",
+    "url": "/language-app/static/js/main.1fedbe22.chunk.js"
+  },
+  {
+    "revision": "fcdf25039742b5a58f82",
+    "url": "/language-app/static/js/runtime~main.52051548.js"
+  },
+  {
+    "revision": "986a461ddad846ecc6f2",
+    "url": "/language-app/static/js/2.2ccfc8bd.chunk.js"
+  },
+  {
+    "revision": "1e38ecc495746bd18357",
+    "url": "/language-app/static/js/3.12bf67e3.chunk.js"
+  },
+  {
+    "revision": "7aacca8a0fb46881c4b9f46604a14484",
+    "url": "/language-app/index.html"
+  }
+];
